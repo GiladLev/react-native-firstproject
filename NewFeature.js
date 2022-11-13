585a960ddc2aa99c78d4bbbda933b5bfe3b5817a
GiladLev <giladlev2000@gmail.com>
2022-11-13 21:08:43 +0200

    push newFeature

---
+++ NewFeature.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NewFeature = () => {
+  return (
+    <View>
+      <Text>NewFeature</Text>
+    </View>
+  )
+}
+
+export default NewFeature
